de3c795c94381d234c6bf2f505411776bdd2b266
Cameron Batt <vespertilian@gmail.com>
2019-10-14 23:06:41 +1100

    chore(demo.ts): update demo code to match updated api

--- src/demo.ts
+++ src/demo.ts
@@ -26,6 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
         countDownCallback: countDownCallback,
         countDownCancelledCallback: countDownCancelledCallback
     };
-    new InactivityCountdownTimer(settings)
+
+    new InactivityCountdownTimer(settings).start();
 });
 
